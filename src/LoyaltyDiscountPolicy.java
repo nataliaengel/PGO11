ffa10d774adf6b9daef4d70a0c94a4b1a7eaf12f
nataliaengel <s36411@pjwstk.edu.pl>
2026-06-18 21:50:39 +0200

    Update LoyaltyDiscountPolicy.java

--- src/LoyaltyDiscountPolicy.java
+++ src/LoyaltyDiscountPolicy.java
@@ -1,4 +1,4 @@
-// Znizka lojalnosciowa: studenci z co najmniej 100 punktami dostaja 10% taniej.
+
 public class LoyaltyDiscountPolicy implements DiscountPolicy {
     @Override
     public double applyDiscount(Student student, double price) {
